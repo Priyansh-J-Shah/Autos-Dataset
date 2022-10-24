7dfc013517add2714e28a6c56ed0bc9fa0537786
Priyansh-J-Shah <spriyansh78@gmail.com>
2022-10-24 20:03:21 +0530

    Project Configuration

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+Auto_CSV_FILE_PATH = os.path.join("Model","autos_dataset.csv")
+
+Auto_MODEL_FILE_PATH = os.path.join("Model","Linear_Regression_Auto_Dataset.pkl")
+
+Auto_JSON_FILE_PATH = os.path.join("Model","Linear_Regression_Auto_Dataset.json")
+
+PORT_NUMBER = 5500
